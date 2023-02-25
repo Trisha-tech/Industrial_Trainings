7ae8d32ce35b02142d507d1197faffd0c67ec568
Trisha-tech <1234trishasahu@gmail.com>
2023-02-25 21:22:04 +0530

    Added mern stack training details

---
+++ MERN-STACK/NodeJS-Backend/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://trisha:<password>@cluster0.mc8nq.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET: "tsdrfeuyfjdbvsdjfios",
+    SENDGRID_API: "********************************************",
+    EMAIL: "http://localhost:*****"
+} 
